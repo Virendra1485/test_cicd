3afe239cc97b7e4f972261e4f8263724a76c5682
virenda1485 <virendrasinghrawat1485@gmail.com>
2023-06-27 07:50:52 +0530

    Singview complete

--- account/views.py
+++ account/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse_lazy, reverse
 from django.contrib.gis.geos import Point
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, TemplateView
+from django.contrib import messages
 
 
 class UserSignupView(CreateView):
@@ -20,3 +21,19 @@ class UserSignupView(CreateView):
             user.location = Point(float(longitude), float(latitude))
         user.save()
         return redirect(self.success_url)
+    
+class UserSigninView(LoginView):
+    template_name = 'user/login.html'
+
+    def get_success_url(self):
+        return reverse('profile', kwargs={'pk': self.request.user.pk})
+
+    def form_valid(self, form):
+        user = form.get_user()
+        if user is not None:
+            login(self.request, user)
+            return super().form_valid(form)
+        else:
+            form.add_error(None, 'Invalid username or password.')
+            messages.error(self.request, 'Invalid username or password.')
+            return self.form_invalid(form)
