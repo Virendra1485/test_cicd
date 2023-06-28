7e4dd0e9d6ee6189962ac2046cc457718cad80aa
virenda1485 <virendrasinghrawat1485@gmail.com>
2023-06-28 10:51:01 +0530

    use black

--- account/tests.py
+++ account/tests.py
@@ -20,14 +20,14 @@ from .views import UserSigninView
 
 class UserSignupViewTestCase(TestCase):
     def setUp(self):
-        self.signup_url = reverse('signup')
-        self.signin_url = reverse('signin')
+        self.signup_url = reverse("signup")
+        self.signin_url = reverse("signin")
         self.user_data = {
-            'username': 'testuser',
-            'password1': 'testpassword',
-            'password2': 'testpassword',
-            'longitude': '123.456',
-            'latitude': '78.910'
+            "username": "testuser",
+            "password1": "testpassword",
+            "password2": "testpassword",
+            "longitude": "123.456",
+            "latitude": "78.910",
         }
 
     def test_signup_view_success(self):
@@ -36,20 +36,22 @@ class UserSignupViewTestCase(TestCase):
         self.assertRedirects(response, self.signin_url)
 
         User = get_user_model()
-        user = User.objects.get(username=self.user_data['username'])
-        self.assertEqual(user.location.x, float(self.user_data['longitude']))
-        self.assertEqual(user.location.y, float(self.user_data['latitude']))
+        user = User.objects.get(username=self.user_data["username"])
+        self.assertEqual(user.location.x, float(self.user_data["longitude"]))
+        self.assertEqual(user.location.y, float(self.user_data["latitude"]))
 
     def test_signup_view_invalid_form(self):
         invalid_data = {
-            'username': 'testuser',
-            'password1': 'testpassword',
-            'password2': 'testpassword',
+            "username": "testuser",
+            "password1": "testpassword",
+            "password2": "testpassword",
         }
         response = self.client.post(self.signup_url, data=invalid_data)
         self.assertEqual(response.status_code, 200)
-        self.assertFormError(response, 'form', 'longitude', 'This field is required.')
-        self.assertFormError(response, 'form', 'latitude', 'This field is required.')
+        self.assertFormError(response, "form", "longitude", "This field is required.")
+        self.assertFormError(response, "form", "latitude", "This field is required.")
 
         User = get_user_model()
-        self.assertFalse(User.objects.filter(username=self.user_data['username']).exists())
+        self.assertFalse(
+            User.objects.filter(username=self.user_data["username"]).exists()
+        )
--- account/views.py
+++ account/views.py
@@ -10,23 +10,24 @@ from django.contrib import messages
 
 class UserSignupView(CreateView):
     form_class = SignupForm
-    success_url = reverse_lazy('signin')
-    template_name = 'user/signup.html'
+    success_url = reverse_lazy("signin")
+    template_name = "user/signup.html"
 
     def form_valid(self, form):
         user = form.save(commit=False)
-        longitude = form.cleaned_data.pop('longitude')
-        latitude = form.cleaned_data.pop('latitude')
+        longitude = form.cleaned_data.pop("longitude")
+        latitude = form.cleaned_data.pop("latitude")
         if latitude and longitude:
             user.location = Point(float(longitude), float(latitude))
         user.save()
         return redirect(self.success_url)
-    
+
+
 class UserSigninView(LoginView):
-    template_name = 'user/login.html'
+    template_name = "user/login.html"
 
     def get_success_url(self):
-        return reverse('profile', kwargs={'pk': self.request.user.pk})
+        return reverse("profile", kwargs={"pk": self.request.user.pk})
 
     def form_valid(self, form):
         user = form.get_user()
@@ -34,6 +35,6 @@ class UserSigninView(LoginView):
             login(self.request, user)
             return super().form_valid(form)
         else:
-            form.add_error(None, 'Invalid username or password.')
-            messages.error(self.request, 'Invalid username or password.')
+            form.add_error(None, "Invalid username or password.")
+            messages.error(self.request, "Invalid username or password.")
             return self.form_invalid(form)
